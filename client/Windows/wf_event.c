11a89b0e51bd903f3f17058c4e6ca81b196a7d22
garbb <e456h@yahoo.com>
2022-10-08 12:30:29 -0700

    fix keyboard still hooked on minimize via taskbar click

--- client/Windows/wf_event.c
+++ client/Windows/wf_event.c
@@ -1018,7 +1018,7 @@ LRESULT CALLBACK wf_event_proc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
 			
 			last_NCACTIVATE_wParam = wParam;
 			
-			if (wParam && (wfc->hwnd == GetForegroundWindow()))
+			if (wParam)
 			{
 				WLog_DBG("wf_event", "WM_NCACTIVATE:fActive=true");
 				
